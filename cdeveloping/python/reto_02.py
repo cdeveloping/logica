51d65597878cde57b04d6b7d968966685f650896
Carlos <carlosdeveloper@outlook.es>
2024-06-20 19:01:11 +0200

    Reto 2
    Creación del reto 2 de programación

---
+++ cdeveloping/python/reto_02.py
@@ -0,0 +1,48 @@
+import math
+# Segundo reto de programación de Mouredev
+
+"""
+Ejercicio:
+
+ * - Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
+ *   Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits...
+ *   (Ten en cuenta que cada lenguaje puede poseer unos diferentes)
+ * - Utilizando las operaciones con operadores que tú quieras, crea ejemplos
+ *   que representen todos los tipos de estructuras de control que existan
+ *   en tu lenguaje:
+ *   Condicionales, iterativas, excepciones...
+ * - Debes hacer print por consola del resultado de todos los ejemplos.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea un programa que imprima por consola todos los números comprendidos
+ * entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
+ *
+ * Seguro que al revisar detenidamente las posibilidades has descubierto algo nuevo.
+
+"""
+
+print("Los operadores aritméticos en Python son: ")
+print("+ Función Sumar.")
+print("- Función restar.")
+print("* Función multiplicar.")
+print("Función dividir.")
+print("% Módulo, devolverá el resto de una división.")
+print("** Exponente, Exponencial de un número.")
+print("// División: Devuelve el entero de la misma.")
+
+
+a = 10
+b = 15
+
+
+print(f"El valor de a es {a} y el de b es: {b}")
+
+print ("la suma de a y b son: ", a + b)
+print ("La resta de a y b es: ", a - b)
+print ("La multiplicación de a y b es: ", a * b)
+print ("La división de a entre b es: ", a / b)
+print ("El resto de la división entra a y b es:  ", a % b)
+print ("El cuadrado de a es: ", a ** 2)
+print ("El entero de la división entre a y b es: ", a // b)
+
+
