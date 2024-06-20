3479f24d8c35d9151d69a37f038377a1cec1e864
Carlos <carlosdeveloper@outlook.es>
2024-06-20 18:24:15 +0200

    Reto 01
    Primer reto de los ejercicios de programación de moruedev

---
+++ cdeveloping/python/reto_01.py
@@ -0,0 +1,16 @@
+#Creamos el primer comentario en Python y añadimos la url www.python.org
+
+"""
+Esto es un comentario en varias líneas
+"""
+
+'''
+Esto también es un comentario
+'''
+
+my_var = "Mi Variable"
+
+my_int = 3
+my_float = 3,14
+my_bool = True
+my_string = "Mi cadena de texto"
