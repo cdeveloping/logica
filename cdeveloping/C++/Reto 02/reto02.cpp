2a9eed268996238cbe91ce2a135f7f963e30c131
Carlos <carlosdeveloper@outlook.es>
2024-06-20 20:53:27 +0200

    Reto 2 Mouredev
    Segundo reto de mouredev en C++

---
+++ cdeveloping/C++/Reto 02/reto02.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "En este ejercicio hay que crear ejemplos con todo los operadores del lenguaje";
+
+    cout << "*** Aritméticos ***" << endl;
+
+    cout << "La suma de 10 + 15 es: " << 10 + 15 << endl;
+
+    cout << "La resta de 15 - 10 es: " << 15 - 10 << endl;
+
+    cout << "La multiplicación de 10 * 8 es: " << 10 * 8 << endl;
+
+    cout << "La división de 9 entre 3 es: " << 9 / 3 << endl;
+
+    cout << "El resto de la división entre 8 y 3 es: " << 8 % 3 << endl;
+
+    cout << "*** Lógicos ***" << endl;
+
+    bool esVerdadero = !false;
+    bool esFalso = !true;
+
+    cout << "El valor de esVerdadero es: " << esVerdadero << endl;
+
+    cout << "El valor de esFalso es: " << esFalso << endl;
+
+    cout << "*** DE COMPARACIÓN ***" << endl;
+
+    cout << "Igualdad (==)" << endl;
+
+    cout << "Desigualdad (!=)" << endl;
+
+    cout << "Mayor que (>)" << endl;
+
+    cout << "Menor que (<)" << endl;
+
+    cout << "Mayor o igual que (>=)" << endl;
+
+    cout << "Menor o igual que (<=)" << endl;
+
+    cout << "*** Asignación ***" << endl;
+
+    cout << "Asignación simple (=)" << endl;
+
+    cout << "Asignación compuesta ( +=, -=, *=, /=, %=)" << endl;
+
+    cout << "*** UNARIOS ***" << endl;
+
+    cout << "Pre-Incremento (++, ++)" << endl;
+
+    cout << "Pre-Decremento (-,-)" << endl;
+
+    cout << "Post-Incremento (x++)" << endl;
+
+    cout << "Post-Decremento (x-) " << endl;
+
+    
+
+
+}
